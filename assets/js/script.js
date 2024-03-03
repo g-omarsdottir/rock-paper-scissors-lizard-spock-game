3cd4e3b5fbbd208d757e5e65ee3eb1a41d6bd315
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-03-03 13:52:42 +0000

    Fix html and javascript: focusable submit username button
    - Fix html: button type=”submit” instead of type=”button” to be focusable using keyboard
    - Fix javascript: event listener “submit” instead of “click”
    - Fix javascript: add event.preventDefault(); to event listener for submit username button to prevent form submission before javascript can process username submission
    - Add javascript: variable usernameForm as DOM reference (to use with event listener “submit”)

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,26 @@ let computerChoiceElement = document.getElementById("computer-choice");
 let resultElement = document.getElementById("result");
 // Detailled result feedback referencing game rules. Constant variable because game rules are unchangeable.
 const winConditions = {
-  Rock: { Scissors: "crushes", Lizard: "crushes" },
-  Paper: { Rock: "covers", Spock: "disproves" },
-  Scissors: { Paper: "cuts", Lizard: "decapitates" },
-  Lizard: { Spock: "poisons", Paper: "eats" },
-  Spock: { Scissors: "smashes", Rock: "vaporizes" }
+  Rock: {
+    Scissors: "crushes",
+    Lizard: "crushes"
+  },
+  Paper: {
+    Rock: "covers",
+    Spock: "disproves"
+  },
+  Scissors: {
+    Paper: "cuts",
+    Lizard: "decapitates"
+  },
+  Lizard: {
+    Spock: "poisons",
+    Paper: "eats"
+  },
+  Spock: {
+    Scissors: "smashes",
+    Rock: "vaporizes"
+  }
 };
 
 // Scoreboard variables for functions to increment scores and display updated scores in the DOM
@@ -22,7 +37,7 @@ let userScore = 0;
 let userScoreElement = document.getElementById("user-score");
 let computerScore = 0;
 let computerScoreElement = document.getElementById("computer-score");
- 
+
 // Flow of the game: variables for displaying or hiding sections according to logical flow of the game.
 const landingSection = document.getElementById("landing-section");
 const gameSection = document.getElementById("game-section");
@@ -37,51 +52,55 @@ completedSection.style.display = "none";
 // When buttons are clicked, the function compareChoices, the game logic, is called.
 // The string for each choice button from the array choices is passed as an argument to the function compareChoices.
 document.getElementById("rock").addEventListener("click", function () {
-    compareChoices("Rock");
-  });
-  
-  document.getElementById("paper").addEventListener("click", function () {
-    compareChoices("Paper");
-  });
-  
-  document.getElementById("scissors").addEventListener("click", function () {
-    compareChoices("Scissors");
-  });
-  
-  document.getElementById("lizard").addEventListener("click", function () {
-    compareChoices("Lizard");
-  });
-  
-  document.getElementById("spock").addEventListener("click", function () {
-    compareChoices("Spock");
-  });
+  compareChoices("Rock");
+});
 
-  // Section for collecting username. To-do: place variables with other variables when code successfully completed.
-  //Cache element references for performance
-  let usernameInput = document.getElementById("username-input"); // Represents the raw input (temporary) value of input element (subject to change as user types). For initial validation of allowed character limit. Is passed as argument to function displayUsername to store and display in DOM.
-  let username = document.getElementById("username"); // Stores the validated username for function displayUsername.
-  username.textContent = localStorage.getItem(username); // To display the most recent stored value of username in DOM. To-do: move in function to display message at game completion.
+document.getElementById("paper").addEventListener("click", function () {
+  compareChoices("Paper");
+});
 
-  // Event listener when user clicks button "submit username". To validate username and to trigger the DOM display and storage of the validated username.
-  document.getElementById("submit").addEventListener("click", function () {
-    if (usernameInput.value.length > 10 || usernameInput.value.length < 1) {
-      alert("Please choose a username between 1 and 10 characters.");
-      return false; // to prevent submission if username is invalid (more than 10 characters)
-      // less than 1 character is handled with the required attribute in the html input field for username.
-    } else {
+document.getElementById("scissors").addEventListener("click", function () {
+  compareChoices("Scissors");
+});
+
+document.getElementById("lizard").addEventListener("click", function () {
+  compareChoices("Lizard");
+});
+
+document.getElementById("spock").addEventListener("click", function () {
+  compareChoices("Spock");
+});
+
+// Section for collecting username. To-do: place variables with other variables when code successfully completed.
+//Cache element references for performance
+let usernameInput = document.getElementById("username-input"); // Represents the raw input (temporary) value of input element (subject to change as user types). For initial validation of allowed character limit. Is passed as argument to function displayUsername to store and display in DOM.
+let username = document.getElementById("username"); // Stores the validated username for function displayUsername.
+username.textContent = localStorage.getItem(username); // To display the most recent stored value of username in DOM. To-do: move in function to display message at game completion.
+let usernameForm = document.getElementById("username-form"); // As reference of submission form for event listener "submit" (for validation and trigger local storage of username).
+
+// Event listener when user clicks button "submit username". To validate username, trigger local storage of the validated username and playGame function.
+usernameForm.addEventListener("submit", function (event) {
+  // Prevents the default submission of form before JavaScript can handle the username submission, while allowing submit button to be focusable using type="submit".
+  event.preventDefault();
+  if (usernameInput.value.length > 10 || usernameInput.value.length < 1) {
+    alert("Please choose a username between 1 and 10 characters.");
+    return false; // to prevent submission if username is invalid (more than 10 characters)
+    // less than 1 character is handled with the required attribute in the html input field for username.
+  } else {
     collectUsername();
     setTimeout(() => {
       playGame();
     }, 500);
   }
-  });
+});
 
 // Function for DOM display and manage local storage of collected username in DOM.
 function collectUsername() {
   localStorage.setItem(username, usernameInput.value); // To store the username in local storage
-  }
+}
 
-function playGame(){
+// Displays the game section of index.html to allow user to play the game and hides the landing page section and game completed section for a logical flow of the website.
+function playGame() {
   landingSection.style.display = "none";
   gameSection.style.display = "block";
   completedSection.style.display = "none";
@@ -113,7 +132,7 @@ function updateScoreElement(element, score) {
 // Function to update the displayed user and computer choices in the DOM.
 function updateChoiceElements(userChoiceElement, computerChoiceElement, userChoice, computerChoice) {
   userChoiceElement.innerHTML = "Your choice:" + "<br>" + userChoice;
-  computerChoiceElement.innerHTML = "Computer choice:" + "<br>"  + computerChoice;
+  computerChoiceElement.innerHTML = "Computer choice:" + "<br>" + computerChoice;
 }
 
 // Function to update the displayed game result (win, lose, tie) in the DOM.
@@ -123,9 +142,9 @@ function updateResultElement(resultElement, result) {
 
 // Functions for actions depending on results of compareChoices
 function userWins(userChoice, computerChoice) {
-  incrementUserScore();  
-  const reason = winConditions[userChoice][computerChoice]; 
-  const resultMessageWins = userChoice + " " + reason + " " + computerChoice + "!" + "<br>" + " You win!" ;
+  incrementUserScore();
+  const reason = winConditions[userChoice][computerChoice];
+  const resultMessageWins = userChoice + " " + reason + " " + computerChoice + "!" + "<br>" + " You win!";
   updateResultElement(result, resultMessageWins);
 }
 
@@ -135,32 +154,32 @@ function userTies(userChoice, computerChoice) {
 }
 
 function userLoses(userChoice, computerChoice) {
-    incrementComputerScore();
-    const reason = winConditions[computerChoice][userChoice]; // Note the switched order
-    const resultMessageLoses = computerChoice + " " + reason + " " + userChoice + "!" + "<br>" + " You lose!";
-    updateResultElement(result, resultMessageLoses);
+  incrementComputerScore();
+  const reason = winConditions[computerChoice][userChoice]; // Note the switched order
+  const resultMessageLoses = computerChoice + " " + reason + " " + userChoice + "!" + "<br>" + " You lose!";
+  updateResultElement(result, resultMessageLoses);
 }
 
 // Function to compare choices based on game rules
 function compareChoices(userChoice) {
   let computerChoice = generateComputerChoice();
   updateChoiceElements(userChoiceElement, computerChoiceElement, userChoice, computerChoice);
-    if (userChoice === computerChoice) {
-        userTies(userChoice, computerChoice);
-    } else if (
-      (userChoice === "Rock" &&
-        (computerChoice === "Scissors" || computerChoice === "Lizard")) ||
-      (userChoice === "Paper" &&
-        (computerChoice === "Rock" || computerChoice === "Spock")) ||
-      (userChoice === "Scissors" &&
-        (computerChoice === "Paper" || computerChoice === "Lizard")) ||
-      (userChoice === "Lizard" &&
-        (computerChoice === "Paper" || computerChoice === "Spock")) ||
-      (userChoice === "Spock" &&
-        (computerChoice === "Scissors" || computerChoice === "Rock"))
-    ) {
-  userWins(userChoice, computerChoice);
-    } else {
-        userLoses(userChoice, computerChoice);
+  if (userChoice === computerChoice) {
+    userTies(userChoice, computerChoice);
+  } else if (
+    (userChoice === "Rock" &&
+      (computerChoice === "Scissors" || computerChoice === "Lizard")) ||
+    (userChoice === "Paper" &&
+      (computerChoice === "Rock" || computerChoice === "Spock")) ||
+    (userChoice === "Scissors" &&
+      (computerChoice === "Paper" || computerChoice === "Lizard")) ||
+    (userChoice === "Lizard" &&
+      (computerChoice === "Paper" || computerChoice === "Spock")) ||
+    (userChoice === "Spock" &&
+      (computerChoice === "Scissors" || computerChoice === "Rock"))
+  ) {
+    userWins(userChoice, computerChoice);
+  } else {
+    userLoses(userChoice, computerChoice);
   }
 }
